2c3a1ccd831d87b55d3f93fb5505d26710bc1722
DOMEY <dimitri.domey@gmail.com>
2015-04-02 09:56:07 +0200

    SearchNanny : Retrieve message

--- src/fr/iut/allonounou/SearchNannyActivity.java
+++ src/fr/iut/allonounou/SearchNannyActivity.java
@@ -1,6 +1,7 @@
 package fr.iut.allonounou;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SearchNannyActivity extends Activity {
@@ -9,5 +10,9 @@ public class SearchNannyActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_searchnanny);
+		
+		Intent intent = getIntent();
+		String location = intent.getStringExtra(MainActivity.EXTRA_LOCATION);
+		String nannyName = intent.getStringExtra(MainActivity.EXTRA_NANNY_NAME);
 	}
 }
