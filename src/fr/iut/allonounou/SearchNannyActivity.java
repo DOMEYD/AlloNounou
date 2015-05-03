34047d739df613c3bc76c954ca8865d8ea598925
DOMEY <dimitri.domey@gmail.com>
2015-05-03 13:11:18 +0200

    - PASSED datas

--- src/fr/iut/allonounou/SearchNannyActivity.java
+++ src/fr/iut/allonounou/SearchNannyActivity.java
@@ -5,14 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class SearchNannyActivity extends Activity {
+	
+	private String nannyName;
+	private double[] location;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 		
+		// RETRIEVE passed datas
 		Intent intent = getIntent();
-		String location = intent.getStringExtra(MainActivity.EXTRA_LOCATION);
-		String nannyName = intent.getStringExtra(MainActivity.EXTRA_NANNY_NAME);
+		Bundle b = intent.getExtras();
+		location = b.getDoubleArray(MainActivity.EXTRA_LOCATION);
+		nannyName = intent.getStringExtra(MainActivity.EXTRA_NANNY_NAME);
 	}
 }
