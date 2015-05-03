5738a4c008d503725de66a49f2ecbfb973dbf123
DOMEY <dimitri.domey@gmail.com>
2015-05-03 12:46:17 +0200

    - GET address from lat lon - CREATE dialog for waiting addresse (no link to code)

--- src/fr/iut/allonounou/MainActivity.java
+++ src/fr/iut/allonounou/MainActivity.java
@@ -1,8 +1,14 @@
 package fr.iut.allonounou;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,6 +21,10 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 	public final static String EXTRA_LOCATION = "fr.iut.allonounou.LOCATION";
 	public final static String EXTRA_NANNY_NAME = "fr.iut.allonounou.NANNY_NAME";
+	
+	private double lat = 0;
+	private double lon = 0;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,47 +52,53 @@ public class MainActivity extends Activity {
 	}
 	
 	public void searchNanny(View view) {
+		// GET intent for search nanny
 		Intent intent = new Intent(this, SearchNannyActivity.class);
 		
-		// MOVE Location TO new activity
-	    EditText editText = (EditText) findViewById(R.id.et_location);
-	    String message = editText.getText().toString();
-	    intent.putExtra(EXTRA_LOCATION, message);
-	    // MOVE Nanny name TO new activity
-	    EditText editText2 = (EditText) findViewById(R.id.et_assistName);
-	    String message2 = editText2.getText().toString();
-	    intent.putExtra(EXTRA_NANNY_NAME, message2);
+		EditText editTextnannyName = (EditText) findViewById(R.id.et_assistName);
+	    String nannyName = editTextnannyName.getText().toString();
+		if(nannyName.length() > 0) {
+			intent.putExtra(EXTRA_NANNY_NAME, nannyName);
+		} else if(this.lat != 0 && this.lon != 0) {
+			Double [] pos = {lat, lon};
+			intent.putExtra(EXTRA_LOCATION, pos);
+		} else {
+			return;
+		}
+		
+//		// MOVE Location TO new activity
+//	    EditText editText = (EditText) findViewById(R.id.et_location);
+//	    String message = editText.getText().toString();
+//	    intent.putExtra(EXTRA_LOCATION, message);
+//	    // MOVE Nanny name TO new activity
+//	    EditText editText2 = (EditText) findViewById(R.id.et_assistName);
+//	    String message2 = editText2.getText().toString();
+//	    intent.putExtra(EXTRA_NANNY_NAME, message2);
 	    
 	    // LAUNCH
 	    startActivity(intent);
 	}
 	
-	public void getLocation(View view) {
-		// Acquire a reference to the system Location Manager
+	public void getLocation(View view) throws IOException {
+		// GET locationManager
 		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-//		// Define a listener that responds to location updates
-//		LocationListener locationListener = new LocationListener() {
-//		    public void onLocationChanged(Location location) {
-//		      // Called when a new location is found by the network location provider.
-//		      makeUseOfNewLocation(location);
-//		    }
-//
-//		    public void onStatusChanged(String provider, int status, Bundle extras) {}
-//
-//		    public void onProviderEnabled(String provider) {}
-//
-//		    public void onProviderDisabled(String provider) {}
-//		};
-//
-//		// Register the listener with the Location Manager to receive location updates
-//		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 		
+		// RETRIEVE last location (know) from network
 		Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 		
-	    EditText editText = (EditText) findViewById(R.id.et_location);
-		EditText editText2 = (EditText) findViewById(R.id.et_assistName);
-		editText.setText("" + lastKnownLocation.getLatitude());
-		editText2.setText("" + lastKnownLocation.getLongitude());
+		// SET latitude, longitude
+		lat = lastKnownLocation.getLatitude();
+		lon = lastKnownLocation.getLongitude();
+		
+		// GET geocoder service
+		Geocoder geocoder;
+		List<Address> addresses;
+		geocoder = new Geocoder(this, Locale.getDefault());
+		
+		// RETRIEVE list of matchs addresses
+		addresses = geocoder.getFromLocation(lat, lon, 1);
+		
+		EditText editTextLocation = (EditText) findViewById(R.id.et_location);
+		editTextLocation.setText(addresses.get(0).getAddressLine(0) + " " + addresses.get(0).getAdminArea() + " " + addresses.get(0).getLocality());
 	}
 }
