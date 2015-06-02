9ba6c612e2f8f67efc26b68e387026c06d206a9b
Kasibear <benj-le-kiwi@hotmail.fr>
2015-06-02 20:21:19 +0200

    update database

--- src/fr/iut/allonounou/DBNanny2.java
+++ src/fr/iut/allonounou/DBNanny2.java
@@ -1,56 +1,167 @@
 package fr.iut.allonounou;
 
-import android.database.sqlite.SQLiteOpenHelper;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.Cursor;
-import android.content.Context;
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.util.List;
 import java.util.ArrayList;
 
 
-public class DBNanny2 extends SQLiteOpenHelper {
+public class DBNanny2{
 
-    private static final String DB_NAME = "DB";             // Nom de la base.
-    public static final String DB_TABLE_NAME = "valeurs";  // Nom de la table.
-	public static final String DB_TABLE_PERSONNE = "personne";  // Nom de la table.
-	  
-	public static final String KEY_FIRSTNAME = "nom";
-	public static final String KEY_LASTNAME = "prenom";
+	private static final String TAG = "DBNanny2";
+
+	public static final String KEY_ROWID = "_id";
+	public static final int COL_ROWID = 0;
+	
+	 //clé de la base de donnée
+	public static final String KEY_NAME = "name";
+	public static final String KEY_FREEPLACE = "freeplace";
+	public static final String KEY_ADRESSE = "adresse";
+	public static final String KEY_LONGITUDE = "longitude";
+	public static final String KEY_LATITUDE = "latitude";
+	public static final String KEY_MAIL = "mail";
+	public static final String KEY_PRIX = "prix";
+	public static final String KEY_WORKPLACE = "workplace";
+	public static final String KEY_FAVORI = "favori";
+	
+	// TODO: Setup your field numbers here (0 = KEY_ROWID, 1=...)
+	public static final int COL_NAME = 1;
+	public static final int COL_FREEPLACE = 2;
+	public static final int COL_ADRESSE = 3;
+	public static final int COL_LONGITUDE = 4;
+	public static final int COL_LATITUDE = 5;
+	public static final int COL_MAIL = 6;
+	public static final int COL_PRIX = 7;
+	public static final int COL_WORKPLACE = 8;
+	public static final int COL_FAVORI = 9;
+	
+	public static final String[] ALL_KEYS = new String[] {KEY_ROWID, KEY_NAME, KEY_FREEPLACE, KEY_ADRESSE, KEY_LONGITUDE, KEY_LATITUDE, KEY_MAIL, KEY_PRIX, KEY_WORKPLACE, KEY_FAVORI};
+	
+	public static final String DATABASE_NAME = "MyDb";
+	public static final String DATABASE_TABLE = "mainTable";
+
+	public static final int DATABASE_VERSION = 2;	
+    
+	private static final String DATABASE_CREATE_SQL = 
+			"create table " + DATABASE_TABLE 
+			+ " (" + KEY_ROWID + " integer primary key autoincrement, "
+			+ KEY_NAME + " text not null, "
+			+ KEY_FREEPLACE + " text not null, "
+			+ KEY_ADRESSE + " text not null, "
+			+ KEY_LONGITUDE + " integer not null, "
+			+ KEY_LATITUDE + " integer not null, "
+			+ KEY_MAIL + " text not null, "
+			+ KEY_PRIX + " text not null, "
+			+ KEY_WORKPLACE + " text not null, "
+			+ KEY_FAVORI + " integer not null"
+			+ ");";
+	
+	private final Context context;
+	
+	private DatabaseHelper myDBHelper;
+	private SQLiteDatabase db;
+
+    public DBNanny2(Context ctx) {
+    	this.context = ctx;
+		myDBHelper = new DatabaseHelper(context);
+    }
+    
+	// Open the database connection.
+	public DBNanny2 open() {
+		db = myDBHelper.getWritableDatabase();
+		return this;
+	}
 	
 	
-    private SQLiteDatabase db;                              // Base de données
+	// Close the database connection.
+	public void close() {
+		myDBHelper.close();
+	}
+	
+		
+	public long insertRow(String name, int freeplace, String adresse, int longitude, int latitude, String mail, int prix, String workplace, int favori) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_NAME, name);
+		initialValues.put(KEY_FREEPLACE, freeplace+" place(s) dispo(s)");
+		initialValues.put(KEY_ADRESSE, adresse);
+		initialValues.put(KEY_LONGITUDE, longitude);
+		initialValues.put(KEY_LATITUDE, latitude);
+		initialValues.put(KEY_MAIL, mail);
+		initialValues.put(KEY_PRIX, prix +" €/heure");
+		initialValues.put(KEY_WORKPLACE, workplace);
+		initialValues.put(KEY_FAVORI, favori);
+		// Insert it into the database.
+		return db.insert(DATABASE_TABLE, null, initialValues);
+	}
+	
+	// Delete a row from the database, by rowId (primary key)
+	public boolean deleteRow(long rowId) {
+		String where = KEY_ROWID + "=" + rowId;
+		return db.delete(DATABASE_TABLE, where, null) != 0;
+	}
+	
+	public void deleteAll() {
+		Cursor c = getAllRows();
+		long rowId = c.getColumnIndexOrThrow(KEY_ROWID);
+		if (c.moveToFirst()) {
+			do {
+				deleteRow(c.getLong((int) rowId));				
+			} while (c.moveToNext());
+		}
+		c.close();
+	}
+	
+	
+	public Cursor getAllRows() {
+		String where = null;
+		Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS, 
+							where, null, null, null, null, null);
+		if (c != null) {
+			c.moveToFirst();
+		}
+		return c;
+	}
 
-    DBNanny2(Context context) {
-		// Appel au constructeur qui s'occupe de créer ou ouvrir la base.
-		super(context, DB_NAME, null, 2);
-		// Récupération de la base de données.
-		db = getWritableDatabase();
-    }
-
-    /**
-     * Méthode appelée si la base n'existe pas.
-     */
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-		db.execSQL("create table " + DB_TABLE_NAME+ " (_id integer primary key autoincrement, value text not null);");
-		db.execSQL("create table " + DB_TABLE_PERSONNE+ " (_id integer primary key autoincrement,"+ KEY_LASTNAME +" text not null,"+ KEY_FIRSTNAME +" text not null);");
-    }
-
-    /**
-     * Méthode pour passer d'une version de SQLite à une nouvelle version.
-     */
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldversion, int newversion) {
-
-    }
+	// Get a specific row (by rowId)
+	public Cursor getRow(String key, String condition) {
+		String where = key + "=" + condition;
+		Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS, 
+						where, null, null, null, null, null);
+		if (c != null) {
+			c.moveToFirst();
+		}
+		return c;
+	}
+	
+	// Change an existing row to be equal to new data.
+		public boolean updateRow(long rowId, String name, String freeplace, String adresse, int longitude, int latitude, String mail, int prix, String workplace, int favori) {
+			String where = KEY_ROWID + "=" + rowId;
+			ContentValues newValues = new ContentValues();
+			newValues.put(KEY_NAME, name);
+			newValues.put(KEY_FREEPLACE, freeplace+" place(s) dispo(s)");
+			newValues.put(KEY_ADRESSE, adresse);
+			newValues.put(KEY_LONGITUDE, longitude);
+			newValues.put(KEY_LATITUDE, latitude);
+			newValues.put(KEY_MAIL, mail);
+			newValues.put(KEY_PRIX, prix +" €/heure");
+			newValues.put(KEY_WORKPLACE, workplace);
+			newValues.put(KEY_FAVORI, favori);
+			// Insert it into the database.
+			return db.update(DATABASE_TABLE, newValues, where, null) != 0;
+		}
+	
+	
+	
 
     /**
      * Insertion d'une chaîne dans la table.
      */
-    public void insertValue(String value) {
+   /* public void insertValue(String value) {
 		// La chaîne n'est pas directement ajoutée dans la base.
 		// Il faut passer par la création d'une structure intermédiaire ContenValues.
 		ContentValues content = new ContentValues();
@@ -58,17 +169,17 @@ public class DBNanny2 extends SQLiteOpenHelper {
 		content.put("value", value);
 
 		// Insertion dans la base de l'instance de ContentValues contenant la chapine.
-		db.insert(DB_TABLE_NAME, null, content);
-    }
+		db.insert(DB_TABLE_NANNY, null, content);
+    }*/
 
     /**
      * Récupération des chaînes de la table.
      */
-    public List<String> getValues() {
+  /*  public List<String> getValues() {
 		List<String> list = new ArrayList<String>();
 		String[] columns = {"value"};
 		// Exécution de la requête pour obtenir les chaînes et récupération d'un curseur sur ces données.
-		Cursor cursor = db.query(DB_TABLE_NAME, columns, null, null, null, null, null);
+		Cursor cursor = db.query(DB_TABLE_NANNY, columns, null, null, null, null, null);
 		// Curseur placé en début des chaînes récupérées.
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
@@ -81,31 +192,28 @@ public class DBNanny2 extends SQLiteOpenHelper {
 		cursor.close();
 
 		return list;
-    }
+    }*/
     
     
-    public void insertPersonne(){
+   /* public void insertPersonnePersonne p(){
 		ContentValues content = new ContentValues();
-		content.put(KEY_LASTNAME,"Jacqueline");
-		content.put(KEY_FIRSTNAME,"TUILARD");
+		content.put"prenom"Mp.getPrenom()");
+		content.put"nom"Mp.getNom()")
+		;
 		db.insert("personne",null,content);
-		content.put(KEY_LASTNAME,"Jacqueline2");
-		content.put(KEY_FIRSTNAME,"TUILARD2");
-		db.insert("personne",null,content);
-	}
+	}*/
     
     
-    public List<String> getPersonne() {
+   /* public List<String> getPersonne() {
 		List<String> list = new ArrayList<String>();
 		String[] columns = {"prenom","nom"};
 		// Exécution de la requête pour obtenir les chaînes et récupération d'un curseur sur ces données.
-		Cursor cursor = db.query(DB_TABLE_PERSONNE, columns, null, null, null, null, null);
+		Cursor cursor = db.query(DB_TABLE_NANNY, columns, null, null, null, null, null);
 		// Curseur placé en début des chaînes récupérées.
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
 			// Récupération d'une chaîne et insertion dans une liste.
 			list.add(cursor.getString(0));
-			list.add(cursor.getString(1));
 			// Passage à l'entrée suivante.
 			cursor.moveToNext();
 		}
@@ -113,18 +221,34 @@ public class DBNanny2 extends SQLiteOpenHelper {
 		cursor.close();
 
 		return list;
-    }
+    }*/
     
- // Return all data in the database.
- 	public Cursor getAllRows() {
- 		String where = null;
- 		List<String> list = new ArrayList<String>();
-		String[] columns = {KEY_LASTNAME,KEY_FIRSTNAME};
- 		Cursor c = 	db.query(true,DB_TABLE_PERSONNE, columns, null, null, null, null, null,null);
- 		if (c != null) {
- 			c.moveToFirst();
- 		}
- 		return c;
- 	}
+		/**
+		 * Private class which handles database creation and upgrading.
+		 * Used to handle low-level database access.
+		 */
+		private static class DatabaseHelper extends SQLiteOpenHelper
+		{
+			DatabaseHelper(Context context) {
+				super(context, DATABASE_NAME, null, DATABASE_VERSION);
+			}
 
-}
+			@Override
+			public void onCreate(SQLiteDatabase _db) {
+				_db.execSQL(DATABASE_CREATE_SQL);			
+			}
+
+			@Override
+			public void onUpgrade(SQLiteDatabase _db, int oldVersion, int newVersion) {
+				Log.w(TAG, "Upgrading application's database from version " + oldVersion
+						+ " to " + newVersion + ", which will destroy all old data!");
+				
+				// Destroy old database:
+				_db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+				
+				// Recreate new database:
+				onCreate(_db);
+			}
+		}
+	}
+
--- src/fr/iut/allonounou/SearchNannyActivity.java
+++ src/fr/iut/allonounou/SearchNannyActivity.java
@@ -1,10 +1,13 @@
 package fr.iut.allonounou;
 
 import java.util.List;
+import android.widget.AdapterView;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -13,28 +16,36 @@ public class SearchNannyActivity extends Activity {
 	
 	private String nannyName;
 	private double[] location;
-	private DBNanny2 dbopenhelper;
+	
+	DBNanny2 myDB;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search);
-		
-		dbopenhelper = new DBNanny2(this);
-	
 		// RETRIEVE passed datas
 		Intent intent = getIntent();
 		Bundle b = intent.getExtras();
 		location = b.getDoubleArray(MainActivity.EXTRA_LOCATION);
 		nannyName = intent.getStringExtra(MainActivity.EXTRA_NANNY_NAME);
+				
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search2);
 		
-		dbopenhelper.insertPersonne();
-		TextView textview = (TextView)findViewById(R.id.title);
-		TextView textview1 = (TextView)findViewById(R.id.artiste);
+		openDB();
+	
+		//initiate DATABASE
+		ClearAll();
+		InitiateBD();
+		
+		populateListViewFromDB();
+		
+		
+		//myDB.insertPersonne();
+		//TextView textview = (TextView)findViewById(R.id.title);
+		//TextView textview1 = (TextView)findViewById(R.id.artiste);
 
 		// Récupération des chaînes dans la base de données.
 		
-		List<String> list = dbopenhelper.getPersonne();
+		//List<String> list = myDB.getPersonne();
 
 		/*String values = "";
 		for(int i = 0 ; i < list.size() ;  ++i) {
@@ -42,23 +53,65 @@ public class SearchNannyActivity extends Activity {
 		}*/
 
 		// Concaténation des chaines pour affichage
-		textview.setText(list.get(0));
-		textview1.setText(list.get(1));
-		
-		//populateListViewFromDB();
+		//textview.setText(list.get(0));
+		//textview1.setText(list.get(1));
 	}
 	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();	
+		closeDB();
+	}
+
+	private void openDB() {
+		myDB = new DBNanny2(this);
+		myDB.open();
+		
+	}
+	private void closeDB() {
+		myDB.close();
+	}
+	
+	/* 
+	 * UI Button Callbacks
+	 */
+	public void InitiateBD() {
+		// nom et prénom, nb de place dipo, indic adress, longitude, latitude, mail, prix, workplace, favori
+		myDB.insertRow("Jacqueline TUILARD" ,1, "quartier Blossière",0,0,"t-benjamin@hotmail.fr",12,"appartement",1);
+		myDB.insertRow("Maelle LEBON" ,3, "près de la gare",0,0,"t-benjamin@hotmail.fr",8,"appartement",0);
+		myDB.insertRow("Lilianne MARECHAL" ,2, "bord de Loire",0,0,"t-benjamin@hotmail.fr",10,"maison",0);
+		myDB.insertRow("Catherine DUPONT" ,0, "Parc des Expositions",0,0,"t-benjamin@hotmail.fr",11,"appartement",0);
+		myDB.insertRow("Joelle MARTIN" ,0, "près de la gare",0,0,"t-benjamin@hotmail.fr",7,"maison",0);
+		myDB.insertRow("Brigitte LEPONDEL" ,1, "près de la gare",0,0,"t-benjamin@hotmail.fr",14,"maison",0);
+		myDB.insertRow("Maelle COURVA" ,1, "quartier Blossière",0,0,"t-benjamin@hotmail.fr",20,"appartement",0);
+		myDB.insertRow("Maelle SILVA" ,2, "bord de Loire",0,0,"t-benjamin@hotmail.fr",15,"appartement",0);
+		myDB.insertRow("Diana MARTIN" ,4, "près de la gare",0,0,"t-benjamin@hotmail.fr",18,"maison",1);
+		
+	}
+	
+	
+	public void ClearAll() {
+		myDB.deleteAll();
+	}
+
+	
 	private void populateListViewFromDB() {
-		Cursor cursor = dbopenhelper.getAllRows();		
-		
+
+		Cursor cursor = myDB.getAllRows();
+
+		// Allow activity to manage lifetime of the cursor.
+		// DEPRECATED! Runs on the UI thread, OK for small/short queries.
 		startManagingCursor(cursor);
-		
+
+		// Setup mapping from cursor to view fields:
 		String[] fromFieldNames = new String[] 
-				{DBNanny2.KEY_LASTNAME, DBNanny2.KEY_FIRSTNAME};
-		
+				{DBNanny2.KEY_NAME,  DBNanny2.KEY_ADRESSE,  DBNanny2.KEY_FREEPLACE};
+	
 		int[] toViewIDs = new int[]
-				{R.id.title,            R.id.artiste};
+				{R.id.title,     R.id.duree,     R.id.artiste};
+
 		
+		// Create adapter to may columns of the DB onto elemesnt in the UI.
 		SimpleCursorAdapter myCursorAdapter = 
 				new SimpleCursorAdapter(
 						this,		// Context
@@ -67,9 +120,11 @@ public class SearchNannyActivity extends Activity {
 						fromFieldNames,			// DB Column names
 						toViewIDs				// View IDs to put information in
 						);
-		
-		/*ListView myList = (ListView) findViewById(R.id.listViewNanny);
-		myList.setAdapter(myCursorAdapter);*/
+
+		// Set the adapter for the list view
+		ListView myList = (ListView) findViewById(R.id.listViewNanny);
+
+		myList.setAdapter(myCursorAdapter);
+
 	}
-	
 }
