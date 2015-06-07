c3ed94593488bc558619fdfdda900003acb17785
Kasibear <benj-le-kiwi@hotmail.fr>
2015-06-07 16:17:54 +0200

    Affinage des infos profil, mise en place de toast d'aide

--- src/fr/iut/allonounou/ProfilActivity.java
+++ src/fr/iut/allonounou/ProfilActivity.java
@@ -9,11 +9,14 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class ProfilActivity extends Activity {
 	
@@ -74,9 +77,9 @@ public class ProfilActivity extends Activity {
 		String word="";
 		int prix=0;
 		
-		int repas=0;  int secours=0;  int nuit=0; 
-		int gouter=0; int chat=0;  int promenade=0;
-		int jeux=0;	  int chien=0; int atelier=0;
+		int repas=0; 	int secours=0;  int nuit=0; 
+		int gouter=0; 	int chat=0;  	int promenade=0;
+		int jeux=0;	  	int chien=0; 	int atelier=0;
 
 		
 		Cursor cursor = myDB.getRow(DBNanny2.KEY_ROWID ,idNanny);
@@ -89,40 +92,40 @@ public class ProfilActivity extends Activity {
 			word = cursor.getString(DBNanny2.COL_WORD);
 			
 				
-			freecat1  = cursor.getInt(DBNanny2.COL_FREECAT1);
-			freecat2  = cursor.getInt(DBNanny2.COL_FREECAT2);
-			freecat3  = cursor.getInt(DBNanny2.COL_FREECAT3);
-			repas  = cursor.getInt(DBNanny2.COL_REPAS);
-			secours  = cursor.getInt(DBNanny2.COL_SECOURS);
-			nuit  = cursor.getInt(DBNanny2.COL_NUIT);
- 			gouter  = cursor.getInt(DBNanny2.COL_GOUTER);
- 			chat  = cursor.getInt(DBNanny2.COL_CHAT);
- 			promenade  = cursor.getInt(DBNanny2.COL_PROMENADE);
-			jeux  = cursor.getInt(DBNanny2.COL_JEUX);
-			chien  = cursor.getInt(DBNanny2.COL_CHIEN);
-			atelier  = cursor.getInt(DBNanny2.COL_CHAT);
+			freecat1  	= cursor.getInt(DBNanny2.COL_FREECAT1);
+			freecat2  	= cursor.getInt(DBNanny2.COL_FREECAT2);
+			freecat3 	= cursor.getInt(DBNanny2.COL_FREECAT3);
+			repas  		= cursor.getInt(DBNanny2.COL_REPAS);
+			secours  	= cursor.getInt(DBNanny2.COL_SECOURS);
+			nuit  		= cursor.getInt(DBNanny2.COL_NUIT);
+ 			gouter  	= cursor.getInt(DBNanny2.COL_GOUTER);
+ 			chat  		= cursor.getInt(DBNanny2.COL_CHAT);
+ 			promenade  	= cursor.getInt(DBNanny2.COL_PROMENADE);
+			jeux  		= cursor.getInt(DBNanny2.COL_JEUX);
+			chien  		= cursor.getInt(DBNanny2.COL_CHIEN);
+			atelier  	= cursor.getInt(DBNanny2.COL_CHAT);
 
 		}
 		cursor.close();
 		
-		TextView name_profil = (TextView)findViewById(R.id.name_profil);
-		TextView freecat1_profil = (TextView)findViewById(R.id.freecat1_profil);
-		TextView freecat2_profil = (TextView)findViewById(R.id.freecat2_profil);
-		TextView freecat3_profil = (TextView)findViewById(R.id.freecat3_profil);
-		TextView adresse_profil = (TextView)findViewById(R.id.adress_profil);
+		TextView name_profil 		= (TextView)findViewById(R.id.name_profil);
+		TextView freecat1_profil 	= (TextView)findViewById(R.id.freecat1_profil);
+		TextView freecat2_profil 	= (TextView)findViewById(R.id.freecat2_profil);
+		TextView freecat3_profil 	= (TextView)findViewById(R.id.freecat3_profil);
+		TextView adresse_profil 	= (TextView)findViewById(R.id.adress_profil);
 		
-		TextView prix_profil =(TextView)findViewById(R.id.prix_profil);
-		TextView word_profil = (TextView)findViewById(R.id.word_profil);
+		TextView prix_profil 		= (TextView)findViewById(R.id.prix_profil);
+		TextView word_profil 		= (TextView)findViewById(R.id.word_profil);
 	
-		TextView repas_profil  =(TextView)findViewById(R.id.repas_profil);
-		TextView secours_profil = (TextView)findViewById(R.id.secours_profil);
-		TextView nuit_profil  =(TextView)findViewById(R.id.nuit_profil);
-		TextView gouter_profil  = (TextView)findViewById(R.id.gouter_profil);
-		TextView chat_profil  =(TextView)findViewById(R.id.chat_profil);
-		TextView promenade_profil  = (TextView)findViewById(R.id.promenade_profil);
-		TextView jeux_profil  = (TextView)findViewById(R.id.jeux_profil);
-		TextView chien_profil  =(TextView)findViewById(R.id.chien_profil);
-		TextView atelier_profil  = (TextView)findViewById(R.id.atelier_profil);
+		TextView repas_profil  		= (TextView)findViewById(R.id.repas_profil);
+		TextView secours_profil 	= (TextView)findViewById(R.id.secours_profil);
+		TextView nuit_profil  		= (TextView)findViewById(R.id.nuit_profil);
+		TextView gouter_profil  	= (TextView)findViewById(R.id.gouter_profil);
+		TextView chat_profil  		= (TextView)findViewById(R.id.chat_profil);
+		TextView promenade_profil  	= (TextView)findViewById(R.id.promenade_profil);
+		TextView jeux_profil  		= (TextView)findViewById(R.id.jeux_profil);
+		TextView chien_profil  		= (TextView)findViewById(R.id.chien_profil);
+		TextView atelier_profil  	= (TextView)findViewById(R.id.atelier_profil);
 		
 		name_profil.setText(name);
 		adresse_profil.setText(adresse);
@@ -143,7 +146,83 @@ public class ProfilActivity extends Activity {
 		if(jeux==1){jeux_profil.setText("oui");}else{ jeux_profil.setText("non");}
 		if(chien==1){chien_profil.setText("oui");}else{ chien_profil.setText("non");}
 		if(atelier==1){atelier_profil.setText("oui");}else{ atelier_profil.setText("non");}
-
+	
+		UserHelper();
+	}
+	
+	public void UserHelper(){
+		ImageView repasImg = (ImageView)findViewById(R.id.img_repas);
+		repasImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Fourni les Repas", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView secoursImg = (ImageView)findViewById(R.id.img_secours);
+		secoursImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Prodige les Secours", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView nuitImg = (ImageView)findViewById(R.id.img_nuit);
+		nuitImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Fait la Garde de Nuit", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView gouterImg = (ImageView)findViewById(R.id.img_gouter);
+		gouterImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Fourni le Gouter", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView chatImg = (ImageView)findViewById(R.id.img_chat);
+		chatImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Possède un Chat", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView promenadeImg = (ImageView)findViewById(R.id.img_promenade);
+		promenadeImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Fait des Promenades", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView jeuxImg = (ImageView)findViewById(R.id.img_jeux);
+		jeuxImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Accès à des Aires de Jeux", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView chienImg = (ImageView)findViewById(R.id.img_chien);
+		chienImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Possède un Chien", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		ImageView activitesImg = (ImageView)findViewById(R.id.img_activites);
+		activitesImg.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Toast.makeText(ProfilActivity.this, "Fait faire des Activités", Toast.LENGTH_SHORT).show();
+			}
+			
+		});
 	}
 		
-	}
+}
